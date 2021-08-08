230d1c0247d615770a737dfd8750bb14b41118f7
a362389462 <1728771259@qq.com>
2021-08-09 04:21:14 +0800

    push

--- set4/exercise1.py
+++ set4/exercise1.py
@@ -19,7 +19,6 @@ if LOCAL != CWD:
 
 def get_some_details():
     """Parse some JSON.
-
     In lazyduck.json is a description of a person from https://randomuser.me/
     Read it in and use the json library to convert it to a dictionary.
     Return a new dictionary that just has the last name, password, and the
@@ -33,18 +32,25 @@ def get_some_details():
          dictionary, you'll need integer indeces for lists, and named keys for
          dictionaries.
     """
-    json_data = open(LOCAL + "/lazyduck.json").read()
 
+    json_data = open(LOCAL + "/lazyduck.json").read()
     data = json.loads(json_data)
-    return {"lastName": None, "password": None, "postcodePlusID": None}
+    lastName = data["results"][0]["name"]["last"]
+    password = data["results"][0]["login"]["password"]
+    postcode = data["results"][0]["location"]["postcode"]
+    id = data["results"][0]["id"]["value"]
+    id = int(id)
+    postcode = int(postcode)
+    postcodePlusID = id + postcode
+    return {"lastName": lastName, "password": password, "postcodePlusID": postcodePlusID}
 
 
 def wordy_pyramid():
     """Make a pyramid out of real words.
-
     There is a random word generator here:
-    https://us-central1-waldenpondpress.cloudfunctions.net/give_me_a_word?wordlength=20
-    The generator takes a single argument, length (`wordlength`) of the word.
+    http://api.wordnik.com/v4/words.json/randomWords?api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5&minLength=10&maxLength=10&limit=1
+    The arguments that the generator takes is the minLength and maxLength of the word
+    as well as the limit, which is the the number of words. 
     Visit the above link as an example.
     Use this and the requests library to make a word pyramid. The shortest
     words they have are 3 letters long and the longest are 20. The pyramid
@@ -71,14 +77,35 @@ def wordy_pyramid():
     "Nereis",
     "Leto",
     ]
-    TIP: to add an argument to a URL, use: ?argName=argVal e.g. &wordlength=
+    TIP: to add an argument to a URL, use: ?argName=argVal e.g. &minLength=
     """
-    pass
-
+    url = "https://us-central1-waldenpondpress.cloudfunctions.net/give_me_a_word?wordlength={len}"
+    min = 3
+    max = 20
+    list0 = []
+    list1 = []
+    list2 = []
+    for i in range(min,max+1):
+        fullurl=url.format(len=i)
+        pull = requests.get(fullurl)   
+        if pull.status_code is 200:         
+            randword = pull.content  
+            if randword is None: 
+                pass
+            else:
+                randword = str(randword)
+                if int(i) % 2 ==0:
+                    list2.append(randword[2:len(randword)-1])
+                else:
+                    list1.append(randword[2:len(randword)-1])
+    list2.reverse()
+    list0.extend(list1)
+    list0.extend(list2)
+    return list0
+    
 
 def pokedex(low=1, high=5):
     """ Return the name, height and weight of the tallest pokemon in the range low to high.
-
     Low and high are the range of pokemon ids to search between.
     Using the Pokemon API: https://pokeapi.co get some JSON using the request library
     (a working example is filled in below).
@@ -91,20 +118,35 @@ def pokedex(low=1, high=5):
          variable and then future access will be easier.
     """
     template = "https://pokeapi.co/api/v2/pokemon/{id}"
+    
+    index = -1
+    tallest = -1
 
-    url = template.format(id=5)
+    for x in range (low, high):
+        url = template.format(base=template,id=x)
+        r = requests.get(url)
+        if r.status_code is 200:
+            data = json.loads(r.text)
+            height = data["height"]
+            if height > tallest:
+                tallest = height
+                index = x
+
+    url = template.format(base=template,id=index)
     r = requests.get(url)
     if r.status_code is 200:
-        the_json = json.loads(r.text)
-    return {"name": None, "weight": None, "height": None}
+        data = json.loads(r.text)
+        answer1 = data["name"]
+        answer2 = data["weight"]
+        answer3 = data["height"]
+    return {"name": answer1, "weight": answer2, "height": answer3}
 
 
 def diarist():
     """Read gcode and find facts about it.
-
     Read in Trispokedovetiles(laser).gcode and count the number of times the
     laser is turned on and off. That's the command "M10 P1".
-    Write the answer (a number) to a file called 'lasers.pew' in the Set4 directory.
+    Write the answer (a number) to a file called 'lasers.pew' in the week4 directory.
     TIP: you need to write a string, so you'll need to cast your number
     TIP: Trispokedovetiles(laser).gcode uses windows style line endings. CRLF
          not just LF like unix does now. If your comparison is failing this
@@ -113,7 +155,11 @@ def diarist():
          the test will have nothing to look at.
     TIP: this might come in handy if you need to hack a 3d print file in the future.
     """
-    pass
+    gcode_data= open(LOCAL + "/Trispokedovetiles(laser).gcode").read()
+    count = gcode_data.count("M10 P1")
+    f = open("week4/lasers.pew", "w+")
+    f.write(str(count))
+    f.close
 
 
 if __name__ == "__main__":
